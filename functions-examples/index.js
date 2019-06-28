67728c0551a68d01ad64cb4de160f8f5312b638e
Miguel Olave <molavec@gmail.com>
2019-06-28 12:16:48 -0400

    Añade ejemplo de short-circuit
    Añade ejemplo de short-circuit

--- functions-examples/index.js
+++ functions-examples/index.js
@@ -136,3 +136,17 @@ fun(punto)
  fun2('Miguel')
  fun2()
 
+/**
+ * Ejemplo 7:
+ *
+ * Short Circuit
+ *
+ * Es una forma simple de utilizar un valor por defecto en caso
+ * de que un parámetro sea nulo.
+ *
+ */
+let fun7 = (title) => {
+  return title || "Título por defecto"
+}
+console.log( fun7("Este es un título"))
+console.log( fun7())
