083690545e6eacd5c8d0fb9acbff449e70f5c3ca
Miguel Olave <molavec@gmail.com>
2019-07-15 10:55:47 -0400

    añade ejemplo10

--- jest/example09.test.js
+++ jest/example09.test.js
@@ -4,6 +4,7 @@
  * Description:
  * beforeEach - Repite una configuración para cada test
  * afterEach - Repite una configuración después de cada test
+ *
  * How execute:
  * $ jest jest/example09.test.js
  * $ yarn jest:example09
@@ -13,14 +14,17 @@
 let cities = ['Santiago', 'Lima', 'Buenos Aires'];
 
 beforeEach(() => {
+  console.log("-> Ejecuta beforeEach")
   cities.push('La Paz') //Añade 'La Paz' en cada test
 });
 
 afterEach(() => {
+  console.log("-> Ejecuta afterEach")
   cities.pop() //Remueve 'La Paz' en cada test (Prueba que pasa cuando comentas esta línea)
 });
 
 test('Test beforeEach (1)', () => {
+  console.log("1.- Ejecuta test (1)")
   // console.log(cities)
   expect(cities.length).toBeGreaterThanOrEqual(4);
 });
@@ -28,6 +32,7 @@ test('Test beforeEach (1)', () => {
 
 
 test('Test beforeEach (2)', () => {
+  console.log("2.- Ejecuta test (2)")
   // console.log(cities)
   expect(cities).toContain('La Paz');
 });
--- jest/example10.test.js
+++ jest/example10.test.js
@@ -1,10 +1,40 @@
 /**
- * example 9:
+ * example 9: One-Time Setup
  *
  * Description:
+ * beforeAll - Ejecuta una vez configuración antes de ejecutar cualquier test
+ * afterAll - Ejecuta una vez configuración después de ejecutar todos los test
  *
  * How execute:
  * $ jest jest/example09.test.js
  * $ yarn jest:example09
  *
  */
+
+
+let cities = ['Santiago', 'Lima', 'Buenos Aires'];
+
+
+beforeAll(() => {
+  console.log("1.- Ejecuta beforeAll")
+  cities.push('La Paz') //Añade 'La Paz' en cada test
+});
+
+afterAll(() => {
+  console.log("4.- Ejecuta afterAll")
+  cities.pop() //Remueve 'La Paz' en cada test (Prueba que pasa cuando comentas esta línea)
+});
+
+test('Test beforeEach (1)', () => {
+  console.log("2.- Ejecuta test (1)")
+  console.log(cities)
+  expect(cities.length).toBeGreaterThanOrEqual(4);
+});
+
+
+
+test('Test beforeEach (2)', () => {
+  console.log("3.- Ejecuta test (2)")
+   console.log(cities)
+  expect(cities).toContain('La Paz');
+});
