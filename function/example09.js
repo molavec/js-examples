fb99b9bd48145cad7a5167dd738b22c3f2dc3d15
Ignacio <redhotnacho@gmail.com>
2019-07-15 12:16:16 -0400

    agregada una function
    operaciones matemáticas básicas

---
+++ function/example09.js
@@ -0,0 +1,24 @@
+/**
+ * example08: Operaciones matemáticas en Función 
+ *
+ * Description:
+ * Un buen resumen de operaciones matemáticas básicas
+ * (resta, suma, multiplicación y división)
+ *
+ * How execute:
+ * node function/example09
+ * $ yarn function:example09
+ */
+
+let numberOne = 60;
+let numberTwo = 100;
+
+let result = numberOne - numberTwo;
+console.log(result);
+result = numberOne + numberTwo;
+console.log(result);
+result = numberOne * numberTwo;
+console.log(result);
+result = numberOne / numberTwo;
+console.log(result);
+
