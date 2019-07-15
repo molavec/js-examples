66f5a9546999b6308a31a899778cef6ba3c71e35
Ignacio <redhotnacho@gmail.com>
2019-07-15 12:43:30 -0400

    corrección example09, function

--- function/example09.js
+++ function/example09.js
@@ -13,6 +13,30 @@
 let numberOne = 60;
 let numberTwo = 100;
 
+
+function restar(a , b) {
+  return a-b
+}
+
+function sumar(a , b) {
+  return a+b
+}
+
+function multiplicar(a , b) {
+  return a*b
+}
+
+function dividir(a , b) {
+  return a/b
+}
+console.log(restar(numberOne, numberTwo));
+console.log(sumar(numberOne, numberTwo));
+console.log(multiplicar(numberOne, numberTwo));
+console.log(dividir(numberOne, numberTwo));
+
+
+
+/*
 let result = numberOne - numberTwo;
 console.log(result);
 result = numberOne + numberTwo;
@@ -21,4 +45,4 @@ result = numberOne * numberTwo;
 console.log(result);
 result = numberOne / numberTwo;
 console.log(result);
-
+*/
